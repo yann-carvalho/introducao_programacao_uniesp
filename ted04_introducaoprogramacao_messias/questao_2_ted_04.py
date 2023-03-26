762c00a2c89b8649cefa4edfa9938bb2fc5f8baf
Yann Carvalho <yanncarvalho95@gmail.com>
2023-03-26 15:40:35 -0300

    alt3

--- ted04_introducaoprogramacao_messias/questao_2_ted_04.py
+++ ted04_introducaoprogramacao_messias/questao_2_ted_04.py
@@ -3,9 +3,9 @@
 
 apple1 = float(1.30)
 apple2 = float(1.00)
-compra = float(input("Insira o número de maçãs compradas: "))
+compra = float(input('Insira o número de maçãs adquiridas: '))
 
 if compra >= 12:
-    print(f"O valor total da sua compra é R$ {apple2 * compra}. Você economizou e pagou apenas R$ 1,00 por unidade de maçã!")
+    print(f'O valor total da sua compra é R$ {apple2 * compra:.2f}. Você economizou e pagou apenas R$ 1,00 por unidade de maçã!')
 else:
-    print(f"O valor total da sua compra é R$ {apple1 * compra}. Você pagou R$ 1,30 por unidade de maçã.")
+    print(f'O valor total da sua compra é R$ {apple1 * compra:.2f}. Você pagou R$ 1,30 por unidade de maçã.')
